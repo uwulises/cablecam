cc80eea7c14388d102fa340e1b37dabe853a260d
uwulises <ulises.campodonico@ug.uchile.cl>
2025-09-28 22:54:00 -0300

    update

--- test_move.py
+++ test_move.py
@@ -4,3 +4,13 @@ cablecam = SerialControl()
 cablecam.open_serial()
 
 cablecam.home()
+
+while True:
+    pos = input("Enter position in mm (or 'exit' to quit): ")
+    if pos.lower() == 'exit':
+        break
+    try:
+        mm = float(pos)
+        cablecam.moveto_mm(mm)
+    except ValueError:
+        print("Please enter a valid number.")
