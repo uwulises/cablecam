90b93c1344acc1cc4d1f8c3fb03db5c86cbaad1b
uwulises <ulises.campodonico@ug.uchile.cl>
2025-11-05 23:48:17 -0300

    add homing sequence

--- test_move.py
+++ test_move.py
@@ -3,12 +3,13 @@ from serial_msg.SerialControl import SerialControl
 cablecam = SerialControl()
 cablecam.open_serial()
 
-cablecam.home()
-
 while True:
-    pos = input("Enter position in mm (or 'exit' to quit): ")
+    pos = input("Enter position in mm (or 'exit' to quit or 'home' to go home): ")
     if pos.lower() == 'exit':
         break
+    elif pos.lower() == 'home':
+        cablecam.home()
+        continue
     try:
         mm = float(pos)
         cablecam.moveto_mm(mm)
