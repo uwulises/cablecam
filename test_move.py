6f825a80848c3f42f3a0196420f62adac614e437
uwulises <ulises.campodonico@ug.uchile.cl>
2025-11-06 00:09:38 -0300

    up

--- test_move.py
+++ test_move.py
@@ -9,9 +9,11 @@ while True:
         break
     elif pos.lower() == 'home':
         cablecam.home()
-        continue
     try:
-        mm = float(pos)
-        cablecam.moveto_mm(mm)
+        if float(pos) == mm:
+            print("You are already at this position.")
+        else:
+            mm = float(pos)
+            cablecam.moveto_mm(mm)
     except ValueError:
         print("Please enter a valid number.")
